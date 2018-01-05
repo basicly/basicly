f0208ddf8762134c8888734f1a895407e0e62ff3
Rory Well <rory.e.well@gmail.com>
2018-01-04 22:40:37 -0600

    Add styling to each to do completed entry, and default them to checked

--- client/src/Components/ToDoCompletedEntry/ToDoCompletedEntry.js
+++ client/src/Components/ToDoCompletedEntry/ToDoCompletedEntry.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './ToDoCompletedEntry.css';
 
 const toDoCompletedEntry = (props) => {
   return (
-    <div>
+    <div className="ToDoCompleted">
       <label>
         <input
           type="checkbox"
+          checked
         />
         {props.toDoCompleted.name}
+        <div className="DateContainer">12/25/2017</div>
       </label>
     </div>
   );
--- client/src/Components/ToDoInProgressEntry/ToDoInProgressEntry.js
+++ client/src/Components/ToDoInProgressEntry/ToDoInProgressEntry.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import './ToDoInProgressEntry.css';
 
 const toDoInProgressEntry = (props) => {
   return (
-    <div>
+    <div className="ToDoInProgress">
       <label>
         <input
           type="checkbox"
         />
         {props.toDoInProgress.name}
+        <div className="DateContainer">1/1/2018</div>
       </label>
     </div>
   );
