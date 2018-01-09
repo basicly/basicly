272905f880d6be45565286dd887409ab68610ef3
Rory Well <rory.e.well@gmail.com>
2018-01-08 23:54:58 -0600

    Build out UI for the CreateToDo component and add Due column

--- client/src/Components/CreateToDo/CreateToDo.js
+++ client/src/Components/CreateToDo/CreateToDo.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './CreateToDo.css';
 
+// import the user icon
+// import userIcon from './images/user-icon.png';
+
 const createToDo = (props) => {
   return (
     <div className="createToDo">
       <button className="button">Add To Do</button>
+      {/* <span className="userIconContainer"><img className="userIcon" src={userIcon} alt="User Icon"/></span> */}
+      <span className="due">Due</span>
     </div>
   );
 };
