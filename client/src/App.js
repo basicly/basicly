c0dc39d225354a845aff08c2395b36b4712b3187
sleex89 <samlee723@gmail.com>
2018-01-08 21:38:31 -0800

    Need to debug posting and fetching data.

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,24 @@ import React, { Component } from 'react';
 // import the Layer component
 import './components/Layout/Layout';
 import Layout from './components/Layout/Layout';
+import $ from 'jquery';
 
 class App extends Component {
   // render the component
+  componentDidMount() {
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      url: "http://localhost:3000/inprogress",
+      data: { description: 'hello' },
+      success: function (data) {
+        console.log('SUCCESSFUL', data)
+      },
+      error: function (error) {
+        console.log('ERROR ON POST', error);
+      }
+    })
+  }
   render() {
     return (
       <Layout
