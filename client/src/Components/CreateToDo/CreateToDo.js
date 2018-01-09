e5653262c86fd70a53dbacac14b595687b02248f
Rory Well <rory.e.well@gmail.com>
2018-01-08 22:41:20 -0600

    Combine list and entry components into one and begin redesigning UI

---
+++ client/src/Components/CreateToDo/CreateToDo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CreateToDo.css';
+
+const createToDo = (props) => {
+  return (
+    <div className="createToDo">
+      <button className="button">Add To Do</button>
+    </div>
+  );
+};
+
+export default createToDo;
---
+++ client/src/components/ToDoEntry/ToDoEntry.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ToDoEntry.css';
+
+const toDoEntry = (props) => {
+  let statusColor = null;
+
+  if (props.toDo.isCompleted) {
+    statusColor = 'completed';
+  } else {
+    statusColor = 'inProgress';
+  }
+
+  return (
+    <div className="toDoEntry">
+      <span className={statusColor}>&#10004;</span>
+      <span>{props.toDo.name}</span>
+      <span className="dateContainer">Sep 8</span>
+    </div>
+  );
+};
+
+export default toDoEntry;
