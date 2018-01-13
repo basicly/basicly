3ffaff82f93ede72666bd32d6790d69e2fcd83f9
Jonathan Weinstein <jonathan.davyd.weinstein@gmail.com>
2018-01-12 20:33:20 -0800

    added back in the database_url

--- server/models/index.js
+++ server/models/index.js
@@ -7,21 +7,23 @@ const basename  = path.basename(__filename);
 require('dotenv').config();
 const env = process.env.NODE_ENV || 'development';
 const db = {};
-const dbURL = 'postgres://iqsiekmuhobubu:aeacd4ffc63e884dd379cf22a70796cffa367c8686a20978e5a7b80e0328a6bc@ec2-23-21-246-25.compute-1.amazonaws.com:5432/dcfsvvf408pgf4'
-//if (env === 'production') {
+const dbURL = process.env.DATABASE_URL;
+console.log('This is the database URL', dbURL);
+
+if (env === 'production') {
   const sequelize = new Sequelize(dbURL);
-//} else {
-//   const sequelize = new Sequelize('basicly', process.env.DB_USER, 'password', {
-//     host: 'localhost',
-//     dialect: 'postgres',
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000
-//     }
-//   });  
-// }
+} else {
+  const sequelize = new Sequelize('basicly', process.env.DB_USER, 'password', {
+    host: 'localhost',
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  });  
+}
 
 fs
   .readdirSync(__dirname)
