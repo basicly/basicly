e22c2deb882885c640f3f89b33fb9cd751b381c4
Rory Well <rory.e.well@gmail.com>
2018-01-13 00:01:41 -0600

    Commit again to be able to push changes

--- server/server.js
+++ server/server.js
@@ -9,23 +9,23 @@ const models = require('./models');
 const toggleToDo = require('./routes/ToggleToDo');
 const PORT = process.env.PORT || 5000;
 
-//Body Parser Middleware
+// Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 
-//Set Static Path
+// Set Static Path
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 } else {
     app.use(express.static(__dirname + '/../client/build'));
 }
 
-//Either getting or posting Completed and In Progress To-Do's
+// Either getting or posting Completed and In Progress To-Do's
 app.get('/completed', completed.getCompleted);
 app.post('/inprogress', inProgress.postInProgress);
 app.get('/inprogress', inProgress.getInProgress);
 app.put('/toggleTodo', toggleToDo.ToggleToDo);
 app.put('/deleteTodo', deleteToDo.DeleteToDo)
 
-//Listening to port
+// Listening to port
 app.listen(PORT);
