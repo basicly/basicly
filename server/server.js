6529bd77498df6c9011714959eb7a1a8b98c1548
Jonathan Weinstein <jonathan.davyd.weinstein@gmail.com>
2018-01-12 21:20:01 -0800

    clean up

--- server/server.js
+++ server/server.js
@@ -20,40 +20,11 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(__dirname + '/../client/build'));
 }
 
-//Temporarily adding this back in - Need it to render for current test
-app.get('/api', (req, res) => {
-   res.send(['rory', 'joe', 'boi']);
-});
-
 //Either getting or posting Completed and In Progress To-Do's
 app.get('/completed', completed.getCompleted);
 app.post('/inprogress', inProgress.postInProgress);
 app.get('/inprogress', inProgress.getInProgress);
 app.put('/toggleTodo', toggleToDo.ToggleToDo);
-
-    // models.User.create({
-    //   username: 'samTheUser',
-    //   password: 'haha'
-    // }).then(function() {
-    //   console.log('we did it');
-    //   // res.redirect('/');
-    // });
-
-    // models.ToDo.create({
-    //     UserId: 1,
-    //     isCompleted: false,
-    //     isDeleted: false
-    //   }).then(function() {
-    //     console.log('Todo Created')
-    // });
-
-// models.User.findAll()
-//   .then((data) => {
-//     let date = new Date(data[0].createdAt);
-//     console.log('hello sir', (date.getMonth() + 1) + '/' + date.getDate() + '/' +  date.getFullYear());
-//   });
-
-
 
 //Listening to port
 app.listen(PORT);
